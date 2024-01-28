999766eec078e032aaa47801ba097c837eab8305
Elle'Vera <lyod.knyazh@gmail.com>
2024-01-28 09:47:54 +0530

    p

--- backend/server.js
+++ backend/server.js
@@ -133,7 +133,7 @@ app.use(errorHandler)
 
 
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8000
 const mode = process.env.NODE_ENV
 
 app.listen(PORT,console.log(`Server is running in ${mode} mode on port ${PORT}`))
